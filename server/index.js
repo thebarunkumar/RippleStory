e8a10147f35b960ffc91ea745a1d0b9c0825cb6b
Barun Kumar <77458180+thebarunkumar@users.noreply.github.com>
2025-11-08 20:27:15 +0530

    index.js
    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,6 +20,25 @@ app.use("/api/blog", blogRouter);
 
 const PORT = process.env.PORT || 3000;
 
+// Error Handling
+app.use((err, req, res, next) => {
+  if (err.message === "Not allowed by CORS") {
+    return res.status(403).json({
+      success: false,
+      message: "CORS error: This origin is not allowed.",
+    });
+  }
+
+  if (err.code === "LIMIT_FILE_SIZE") {
+    return res.status(400).json({
+      success: false,
+      message: "File size too large. Max allowed is 5MB.",
+    });
+  }
+  console.error("🔥 Server Error:", err.message);
+  res.status(500).json({ success: false, message: "Internal Server Error" });
+});
+
 // Only start listening when running locally or in non-serverless environments.
 if (!process.env.VERCEL) {
   app.listen(PORT, () => {
